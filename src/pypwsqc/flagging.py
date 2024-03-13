33fb96dcb05fda91c8c71284b484fe96e22191eb
Louise Petersson <louise.petersson@smhi.se>
2024-03-13 11:59:38 +0100

    moved HI-filter to separate function

--- src/pypwsqc/flagging.py
+++ src/pypwsqc/flagging.py
@@ -5,9 +5,10 @@ from __future__ import annotations
 
 import numpy as np
 import numpy.typing as npt
+import xarray as xr
 
 
-def fz_filter(
+def FZ_filter(
     pws_data: npt.NDArray[np.float_], reference: npt.NDArray[np.float_], nint: int = 6
 ) -> npt.NDArray[np.float_]:
     """Flag faulty zeros based on a reference time series.
@@ -67,10 +68,12 @@ def fz_filter(
 
 
 def HI_filter(
-    pws_rain: npt.NDArray[np.float_],
+    pws_data: npt.NDArray[np.float_],
+    nbrs_not_nan: npt.NDArray[np.float_],
     reference: npt.NDArray[np.float_],
     HIthresA: npt.NDArray[np.float_],
     HIthresB: npt.NDArray[np.float_],
+    nstat = npt.NDArray[np.float_], 
 ) -> npt.NDArray[np.float_]:
     """High Influx filter
 
@@ -85,6 +88,8 @@ def HI_filter(
     ----------
     pws_data
         The rainfall time series of the PWS that should be flagged
+    distance_matrix
+        The rainfall time series of the PWS that should be flagged
     reference
         The rainfall time series of the reference, which can be e.g.
         the median of neighboring PWS data within a specified range d
@@ -98,11 +103,13 @@ def HI_filter(
     npt.NDArray
         time series of flags
     """
-    condition1 = ds_pws.HIflag != -1
-    condition2 = (reference < HIthresA) & (pws_rain > HIthresB)
+    HI_array = xr.where(nbrs_not_nan < nstat, -1,0)
+    condition1 = HI_array != -1
+    condition2 = (reference < HIthresA) & (pws_data > HIthresB)
     condition3 = (reference >= HIthresA) & (
-        pws_rain > reference * HIthresB / HIthresA
+        pws_data > reference * HIthresB / HIthresA
     )
-    ds_pws["HIflag"] = xr.where(condition1 & (condition2 | condition3), 1, 0)
+    HI_array = xr.where(condition1 & (condition2 | condition3), 1, 0)
+    print('app')
 
     return HI_array
