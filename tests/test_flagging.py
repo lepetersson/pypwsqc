11a6c7fd3bd75fbc825cfda4592ca0734cbec319
Louise Petersson <louise.petersson@smhi.se>
2024-03-14 14:40:12 +0100

    first simple test for HI filter

--- tests/test_flagging.py
+++ tests/test_flagging.py
@@ -41,3 +41,151 @@ def test_fz_filter():
     # fmt: on
     result = pypwsqc.flagging.fz_filter(pws_data, reference, nint=7)
     np.testing.assert_almost_equal(expected, result)
+
+
+def test_hi_filter():
+    # for max_distance = 10e3
+    # fmt:off
+    pws_data = np.array(
+        [
+            0.0,
+            0.0,
+            0.101,
+            0.0,
+            0.101,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            1.3130001,
+            3.7370002,
+            0.404,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+        ]
+    )
+
+    nbrs_not_nan = np.array(
+        [
+            24,
+            23,
+            24,
+            23,
+            26,
+            23,
+            25,
+            22,
+            23,
+            23,
+            23,
+            23,
+            23,
+            23,
+            22,
+            23,
+            23,
+            26,
+            26,
+            25,
+            25,
+            23,
+            27,
+            25,
+            23,
+        ]
+    )
+
+    reference = np.array(
+        [
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+        ]
+    )
+
+    expected = np.array(
+        [
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+        ]
+    )
+
+    # fmt: on
+    result = pypwsqc.flagging.hi_filter(
+        pws_data=pws_data,
+        nbrs_not_nan=nbrs_not_nan,
+        reference=reference,
+        hi_thres_a=0.4,
+        hi_thres_b=10,
+    )
+    np.testing.assert_almost_equal(expected, result)
+
+    # same test as above but with different `hi_thres_b`
+    # fmt: off
+    expected = np.array([0.       , 0.       , 0.   , 0.       , 0.    , 0.       ,
+       0.       , 0.       , 0.       , 0.       , 0.       , 0.,
+       1, 0.    , 0.       , 0.       , 0.       , 0.       ,
+       0.       , 0.       , 0.       , 0.       , 0.       , 0.       ,
+       0.       ])
+    result = pypwsqc.flagging.hi_filter(pws_data = pws_data,
+                                        nbrs_not_nan = nbrs_not_nan,
+                                        reference = reference,
+                                        hi_thres_a = 0.4,
+                                        hi_thres_b = 3)
+    np.testing.assert_almost_equal(expected, result)
