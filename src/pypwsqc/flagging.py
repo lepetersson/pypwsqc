691c1855aecf94c66084525de7bdad456a74eeda
Louise Petersson <louise.petersson@smhi.se>
2024-06-12 07:51:47 +0200

    solved issue #22, src: -1 added first nint timesteps, changed hints from np to xr

--- src/pypwsqc/flagging.py
+++ src/pypwsqc/flagging.py
@@ -9,8 +9,8 @@ import xarray as xr
 
 
 def fz_filter(
-    pws_data: npt.NDArray[np.float_], nbrs_not_nan: npt.NDArray[np.float_], reference: npt.NDArray[np.float_], nint: int, n_stat=npt.NDArray[np.float_], 
-) -> npt.NDArray[np.float_]:
+    pws_data: xr.DataArray, nbrs_not_nan: xr.DataArray, reference: xr.DataArray, nint: int, n_stat= int, 
+) -> xr.DataArray:
     """Faulty Zeros filter. 
 
     This function applies the FZ filter from the R package PWSQC,
@@ -30,23 +30,23 @@ def fz_filter(
 
     Parameters
     ----------
-    pws_data
+    pws_data (xr.DataArray)
         The rainfall time series of the PWS that should be flagged
-    nbrs_not_nan
+    nbrs_not_nan (xr.DataArray)
         Number of neighbouring stations reporting rainfall
-    reference
+    reference (xr.DataArray)
         The rainfall time series of the reference, which can be e.g.
         the median of neighboring stations
-    nint
+    nint (integer)
         The number of subsequent data points which have to be zero, while
         the reference has values larger than zero, to set the flag for
         this data point to 1.
-    n_stat
+    n_stat (integer)
         Threshold for number of neighbours reporting rainfall
 
     Returns
     -------
-    npt.NDArray
+    xr.DataArray
         time series of flags
     """
     # initialize
@@ -66,31 +66,32 @@ def fz_filter(
     for i in np.arange(len(pws_data)):  
         for j in np.arange(len(pws_data[1])): 
 
-            if j <= nint:
+            if j < nint:
                 fz_array[i, j] = -1
-            if sensor_array[i, j] > 0:
-                fz_array[i, j] = 0
-            elif fz_array[i, j - 1] == 1:
-                fz_array[i, j] = 1
-            elif (np.sum(sensor_array[i, j - nint : j + 1]) > 0) or (
-                np.sum(ref_array[i, j - nint : j + 1]) < nint + 1
-            ):
-                fz_array[i, j] = 0
-            else:
-                fz_array[i, j] = 1
+            else: 
+                if sensor_array[i, j] > 0:
+                    fz_array[i, j] = 0
+                elif fz_array[i, j - 1] == 1:
+                    fz_array[i, j] = 1
+                elif (np.sum(sensor_array[i, j - nint : j + 1]) > 0) or (
+                    np.sum(ref_array[i, j - nint : j + 1]) < nint + 1
+                ):
+                    fz_array[i, j] = 0
+                else:
+                    fz_array[i, j] = 1
 
         fz_array = fz_array.astype(int)
     return xr.where(nbrs_not_nan < n_stat, -1, fz_array)
 
 
 def hi_filter(
-    pws_data: npt.NDArray[np.float_],
-    nbrs_not_nan: npt.NDArray[np.float_],
-    reference: npt.NDArray[np.float_],
-    hi_thres_a: npt.NDArray[np.float_],
-    hi_thres_b: npt.NDArray[np.float_],
-    n_stat=npt.NDArray[np.float_],
-) -> npt.NDArray[np.float_]:
+    pws_data: xr.DataArray,
+    nbrs_not_nan: xr.DataArray,
+    reference: xr.DataArray,
+    hi_thres_a: int,
+    hi_thres_b: int,
+    n_stat= int,
+) -> xr.DataArray:
     """High Influx filter.
 
     This function applies the HI filter from the R package PWSQC,
@@ -109,23 +110,23 @@ def hi_filter(
 
     Parameters
     ----------
-    pws_data
+    pws_data (xr.DataArray)
         The rainfall time series of the PWS that should be flagged
-    nbrs_not_nan
+    nbrs_not_nan (xr.DataArray)
         Number of neighbouring stations reporting rainfall
-    reference
+    reference (xr.DataArray)
         The rainfall time series of the reference, which can be e.g.
         the median of neighboring stations
-    hi_thres_a
+    hi_thres_a (integer)
         Threshold for median rainfall of neighbouring stations [mm]
-    hi_thres_b
+    hi_thres_b (integer)
         Upper rainfall limit [mm]
-    n_stat
+    n_stat (integer)
         Threshold for number of neighbours reporting rainfall
 
     Returns
     -------
-    npt.NDArray
+    xr.DataArray
         time series of flags
     """
     condition1 = (reference < hi_thres_a) & (pws_data > hi_thres_b)
